27bd9bcb7a6e04db7b453d385c87e5d9ea55c421
Daniel Pritchard <daniel.pritchard@aloyoga.com>
2021-08-03 16:37:37 -0700

    more options

--- netlify/functions/shipping.ts
+++ netlify/functions/shipping.ts
@@ -8,27 +8,39 @@ const handler: Handler = async (event, context) => {
             "service_name": "canadapost-overnight",
             "service_code": "ON",
             "total_price": "1295",
-            "description": "This is the fastest option by far",
+            "description": "This will get it there Monday",
             "currency": "CAD",
-            "min_delivery_date": "2022-04-12 14:48:45 -0400",
-            "max_delivery_date": "2022-04-12 14:48:45 -0400"
+            "min_delivery_date": "2022-08-09 08:00:01 -0700",
+            "max_delivery_date": "2022-08-09 08:00:01 -0700"
         },
         {
             "service_name": "fedex-2dayground",
             "service_code": "2D",
             "total_price": "2934",
+            "description": "This will get it there on tuesday",
             "currency": "USD",
-            "min_delivery_date": "2050-04-12 14:48:45 -0400",
-            "max_delivery_date": "2050-04-12 14:48:45 -0400"
+            "min_delivery_date": "2022-08-03 14:48:45 -0700",
+            "max_delivery_date": "2022-08-03 14:48:45 -0700"
         },
         {
             "service_name": "fedex-priorityovernight",
-            "service_code": "1D",
+            "service_code": "ESD",
+            "description": "These are empty strings",
             "total_price": "3587",
             "currency": "USD",
-            "min_delivery_date": "2022-04-12 14:48:45 -0400",
-            "max_delivery_date": "2022-04-12 14:48:45 -0400"
-        }
+            "min_delivery_date": "",
+            "max_delivery_date": ""
+        },
+        {
+          "service_name": "fedex-priorityovernight",
+          "service_code": "3D",
+          "description": "These are plus three days",
+          "total_price": "3587",
+          "currency": "USD",
+          "min_delivery_date": "+3 day",
+          "max_delivery_date": "+3 day"
+      }
+
     ]
  }
   return {
