0cebf98c14c7c36f595da0f21a3222f1f1cc75ca
Daniel Pritchard <daniel.pritchard@aloyoga.com>
2021-08-03 16:13:18 -0700

    Init

---
+++ netlify/functions/shipping.ts
@@ -0,0 +1,40 @@
+import { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, context) => {
+  console.log({event, context});
+  const body = {
+    "rates": [
+        {
+            "service_name": "canadapost-overnight",
+            "service_code": "ON",
+            "total_price": "1295",
+            "description": "This is the fastest option by far",
+            "currency": "CAD",
+            "min_delivery_date": "2022-04-12 14:48:45 -0400",
+            "max_delivery_date": "2022-04-12 14:48:45 -0400"
+        },
+        {
+            "service_name": "fedex-2dayground",
+            "service_code": "2D",
+            "total_price": "2934",
+            "currency": "USD",
+            "min_delivery_date": "2050-04-12 14:48:45 -0400",
+            "max_delivery_date": "2050-04-12 14:48:45 -0400"
+        },
+        {
+            "service_name": "fedex-priorityovernight",
+            "service_code": "1D",
+            "total_price": "3587",
+            "currency": "USD",
+            "min_delivery_date": "2022-04-12 14:48:45 -0400",
+            "max_delivery_date": "2022-04-12 14:48:45 -0400"
+        }
+    ]
+ }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(body),
+  };
+};
+
+export { handler };
